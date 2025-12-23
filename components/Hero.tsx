0ffbfdc1a832a3bc274beae2974f651f5112db11
Saudamini Tergaonkar <saudamini@Saudaminis-MacBook-Air.local>
2025-12-23 16:14:32 +0100

    hero section using aceternity ui unfinished

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AuroraBackground } from './ui/aurora-background'
+
+const Hero = () => {
+    return (
+        <section id="home" className='pb-20 pt-36'>
+            <div>
+                <AuroraBackground />
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
