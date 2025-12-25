e615cbd63e3bc91a84b99d55fd719f5a24f29ede
Saudamini Tergaonkar <saudamini@Saudaminis-MacBook-Air.local>
2025-12-25 17:34:23 +0100

    Updated Hero section - working

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { AuroraBackground } from './ui/aurora-background'
+"use client";
 
-const Hero = () => {
-    return (
-        <section id="home" className='pb-20 pt-36'>
-            <div>
-                <AuroraBackground />
-            </div>
+import { motion } from "framer-motion";import React from "react";
+import { AuroraBackground } from "./ui/aurora-background";
 
-        </section>
-    )
+export function Hero() {
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          We craft intelligent digital experiences
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4 text-center max-w-4xl">
+        Our flagship product, the <b>Webcomposition Architecture Model (WAM)</b> empowers teams to visually model, describe, and refine system architectures through an intuitive AI-assisted platform
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Debug now
+        </button>
+      </motion.div>
+    </AuroraBackground>
+  );
 }
-
-export default Hero
