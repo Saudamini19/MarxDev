bc7b5bfc1b3989c493e871cb2788f2943e3836f8
Saudamini Tergaonkar <saudamini@Saudaminis-MacBook-Air.local>
2025-12-26 16:23:51 +0100

    Navbar elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import DemoOne from "@/components/Hero";
+import { NavbarDemo } from "@/components/Navbar";
+
 
 export default function Home() {
   return (
     <main>
       <DemoOne />
+      <NavbarDemo></NavbarDemo>
     </main>
   );
 }
