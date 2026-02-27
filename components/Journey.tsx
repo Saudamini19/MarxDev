bbf3ba528ade49a525d553467baf1b0542ffd28a
Saudamini Tergaonkar <saudamini.v.tergaonkar@gmail.com>
2026-02-27 14:44:32 +0100

    update: update the progress for February

--- components/Journey.tsx
+++ components/Journey.tsx
@@ -147,15 +147,15 @@ export function TimelineDemo() {
               Ready to go into code freeze.
             </div>
           </div>
-          {/* <div className="grid grid-cols-2 gap-4">
+          { <div className="grid grid-cols-2 gap-4">
             <img
-              src="/TeamWork.jpeg"
+              src="/February.jpg"
               alt="Group Picture of the Team"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
-          </div> */}
+          </div> }
         </div>
       ),
     },
